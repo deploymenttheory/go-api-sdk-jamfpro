d4eaf28e6afc0a2365bb887dcd36e4c085428138
ShocOne <shocone@deploymenttheory.com>
2023-10-13 13:33:27 +0100

    update

--- sdk/jamfpro/shared_api_client_test.go
+++ sdk/jamfpro/shared_api_client_test.go
@@ -12,7 +12,7 @@ func TestOAuthCredentialsSetting(t *testing.T) {
 	// Mock config for testing
 	config := Config{
 		InstanceName:          "testInstance",
-		DebugMode:             true,
+		DebugMode:             false,
 		Logger:                http_client.NewDefaultLogger(),
 		MaxConcurrentRequests: 5,
 		TokenLifespan:         30 * time.Minute,
@@ -21,7 +21,7 @@ func TestOAuthCredentialsSetting(t *testing.T) {
 		ClientSecret:          "mockClientSecret",
 	}
 
-	t.Log("Initializing client with mock configuration...")
+	logger.Info("Initializing client with mock configuration...")
 	client := NewClient(config)
 
 	// Mock new OAuth credentials
@@ -29,13 +29,13 @@ func TestOAuthCredentialsSetting(t *testing.T) {
 		ClientID:     "newMockClientID",
 		ClientSecret: "newMockClientSecret",
 	}
-	t.Logf("Setting client's OAuth credentials to: %v", newCreds)
+	logger.Info("Setting client's OAuth credentials to: %v", newCreds)
 	client.SetClientOAuthCredentials(newCreds)
 
 	// In order to validate the setting of OAuth credentials, the http_client package should expose a method or field to get the current OAuth credentials.
 	// Given that it's private in the current code, you might need to add a getter method in the http_client package.
 	// For now, I'm assuming such a method has been added called GetOAuthCredentials().
 
-	t.Log("Asserting that the client's OAuth credentials have been set correctly...")
+	logger.Info("Asserting that the client's OAuth credentials have been set correctly...")
 	assert.Equal(t, newCreds, client.HTTP.GetOAuthCredentials(), "Expected client to have updated OAuth credentials")
 }
