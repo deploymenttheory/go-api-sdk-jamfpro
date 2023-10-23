fff60020af16bb1ddd9a46e35a61f11f39f5e43e
ShocOne <shocone@deploymenttheory.com>
2023-10-23 08:03:12 +0100

    added examples

--- examples/computer_extension_attributes/ExportComputerExtensionAttributes/ExportComputerExtensionAttributes.go
+++ examples/computer_extension_attributes/ExportComputerExtensionAttributes/ExportComputerExtensionAttributes.go
@@ -35,7 +35,7 @@ func main() {
 	// Configuration for the jamfpro
 	config := jamfpro.Config{
 		InstanceName: authConfig.InstanceName,
-		DebugMode:    true,
+		DebugMode:    false,
 		Logger:       jamfpro.NewDefaultLogger(),
 		ClientID:     authConfig.ClientID,
 		ClientSecret: authConfig.ClientSecret,
@@ -47,6 +47,14 @@ func main() {
 		log.Fatalf("Failed to create Jamf Pro client: %v", err)
 	}
 
+	// Ensure that the export directory exists
+	if _, err := os.Stat(exportPath); os.IsNotExist(err) {
+		err = os.MkdirAll(exportPath, 0755)
+		if err != nil {
+			log.Fatalf("Failed to create export directory: %v", err)
+		}
+	}
+
 	// Get a list of all computer extension attributes
 	attributesList, err := client.GetComputerExtensionAttributes()
 	if err != nil {
@@ -54,6 +62,9 @@ func main() {
 	}
 
 	for _, attributeItem := range attributesList.Results {
+		// Log to indicate which extension attribute is being processed
+		log.Printf("Processing extension attribute '%s' (ID: %d)\n", attributeItem.Name, attributeItem.ID)
+
 		// Get details for each attribute
 		attribute, err := client.GetComputerExtensionAttributeByID(attributeItem.ID)
 		if err != nil {
@@ -61,20 +72,47 @@ func main() {
 			continue
 		}
 
+		// Log the type of the attribute for debugging
+		log.Printf("Attribute '%s' is of type '%s'\n", attribute.Name, attribute.InputType.Type)
+
 		// Check if the type is "Script"
-		if attribute.InputType.Type == "Script" {
+		if strings.ToLower(attribute.InputType.Type) == "script" {
 			// Sanitize the attribute name to be used as a filename
 			sanitizedFileName := sanitizeFileName(attribute.Name)
 
 			// Export the script content
 			scriptFileName := filepath.Join(exportPath, sanitizedFileName+".sh")
 
-			err = os.WriteFile(scriptFileName, []byte(attribute.InputType.Script), 0644)
+			// Log to indicate where the script intends to export the extension attribute
+			log.Printf("Exporting computer extension attribute '%s' to: %s", attribute.Name, scriptFileName)
+
+			// Check if file already exists
+			if _, err := os.Stat(scriptFileName); err == nil {
+				log.Printf("File %s already exists!", scriptFileName)
+			} else if !os.IsNotExist(err) {
+				log.Printf("Error checking if file exists: %v", err)
+			}
+
+			// Use os.Create to create or truncate the file for writing
+			file, err := os.Create(scriptFileName)
+			if err != nil {
+				log.Printf("Failed to create file for attribute '%s': %v", attribute.Name, err)
+				continue
+			}
+			defer file.Close()
+
+			_, err = file.WriteString(attribute.InputType.Script)
 			if err != nil {
 				log.Printf("Failed to write script for attribute '%s' to file '%s': %v", attribute.Name, scriptFileName, err)
 				continue
 			}
 			fmt.Printf("Exported script for attribute '%s' to file '%s'\n", attribute.Name, scriptFileName)
+		} else {
+			log.Printf("Attribute '%s' is not of type 'Script', skipping export.\n", attribute.Name)
+		}
+
+		if err != nil {
+			log.Printf("Error encountered: %v", err)
 		}
 	}
 }
