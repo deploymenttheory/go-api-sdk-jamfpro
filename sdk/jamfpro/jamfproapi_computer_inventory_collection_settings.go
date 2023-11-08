514dc826622acf382b9221aaef424d9fc14c370d
ShocOne <shocone@deploymenttheory.com>
2023-11-08 16:28:16 +0000

    comments tidy up

---
+++ sdk/jamfpro/jamfproapi_computer_inventory_collection_settings.go
@@ -0,0 +1,6 @@
+// jamfproapi_categories.go
+// Jamf Pro Api - osx configuration profiles
+// api reference: https://developer.jamf.com/jamf-pro/reference/get_v1-computer-inventory-collection-settings
+// Jamf Pro API requires the structs to support a JSON data structure.
+
+package jamfpro
