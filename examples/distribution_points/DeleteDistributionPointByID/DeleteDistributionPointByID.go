8fcfc70dab1b547af0f463edf31a3f1d8b339c68
ShocOne <shocone@deploymenttheory.com>
2023-11-10 09:52:39 +0000

    + sdk coverage for distribution points

---
+++ examples/distribution_points/DeleteDistributionPointByID/DeleteDistributionPointByID.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/http_client"
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/GitHub/deploymenttheory/go-api-sdk-jamfpro/clientauth.json"
+
+	// Load the client OAuth credentials from the configuration file
+	authConfig, err := jamfpro.LoadClientAuthConfig(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to load client OAuth configuration: %v", err)
+	}
+
+	// Instantiate the default logger and set the desired log level
+	logger := http_client.NewDefaultLogger()
+	logLevel := http_client.LogLevelDebug
+
+	// Configuration for the jamfpro
+	config := jamfpro.Config{
+		InstanceName: authConfig.InstanceName,
+		LogLevel:     logLevel,
+		Logger:       logger,
+		ClientID:     authConfig.ClientID,
+		ClientSecret: authConfig.ClientSecret,
+	}
+
+	// Create a new jamfpro client instance
+	client, err := jamfpro.NewClient(config)
+	if err != nil {
+		log.Fatalf("Failed to create Jamf Pro client: %v", err)
+	}
+
+	// ID of the distribution point to delete
+	distributionPointID := 1 // Replace with the actual ID
+
+	// Call DeleteDistributionPointByID function
+	err = client.DeleteDistributionPointByID(distributionPointID)
+	if err != nil {
+		log.Fatalf("Error deleting distribution point: %v", err)
+	}
+
+	fmt.Println("Distribution Point deleted successfully")
+}
---
+++ examples/distribution_points/GetDistributionPointByID/GetDistributionPointByID.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/http_client"
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/GitHub/deploymenttheory/go-api-sdk-jamfpro/clientauth.json"
+
+	// Load the client OAuth credentials from the configuration file
+	authConfig, err := jamfpro.LoadClientAuthConfig(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to load client OAuth configuration: %v", err)
+	}
+
+	// Instantiate the default logger and set the desired log level
+	logger := http_client.NewDefaultLogger()
+	logLevel := http_client.LogLevelDebug
+
+	// Configuration for the jamfpro
+	config := jamfpro.Config{
+		InstanceName: authConfig.InstanceName,
+		LogLevel:     logLevel,
+		Logger:       logger,
+		ClientID:     authConfig.ClientID,
+		ClientSecret: authConfig.ClientSecret,
+	}
+
+	// Create a new jamfpro client instance
+	client, err := jamfpro.NewClient(config)
+	if err != nil {
+		log.Fatalf("Failed to create Jamf Pro client: %v", err)
+	}
+
+	// ID of the distribution point to fetch
+	distributionPointID := 1 // Replace with actual ID
+
+	// Call GetDistributionPointByID function
+	distributionPoint, err := client.GetDistributionPointByID(distributionPointID)
+	if err != nil {
+		log.Fatalf("Error fetching distribution point: %v", err)
+	}
+
+	// Pretty print the distribution point in XML
+	distributionPointXML, err := xml.MarshalIndent(distributionPoint, "", "    ")
+	if err != nil {
+		log.Fatalf("Error marshaling distribution point data: %v", err)
+	}
+	fmt.Println("Fetched Distribution Point:\n", string(distributionPointXML))
+}
