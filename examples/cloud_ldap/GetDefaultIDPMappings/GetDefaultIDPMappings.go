7a11056f3d0fd7bc6f4c7c362509396dc4d71d57
Joseph Little <joelittle11@live.com>
2024-01-08 13:10:25 +0000

    CSA Endpoint Complete, examples pending

---
+++ examples/cloud_ldap/GetDefaultIDPMappings/GetDefaultIDPMappings.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/http_client" // Import http_client for logging
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	configFilePath := "/Users/joseph/github/go-api-sdk-jamfpro/clientauth.json"
+
+	authConfig, err := jamfpro.LoadClientAuthConfig(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to load client OAuth configuration: %v", err)
+	}
+
+	logger := http_client.NewDefaultLogger()
+	logLevel := http_client.LogLevelInfo
+	config := jamfpro.Config{
+		InstanceName:       authConfig.InstanceName,
+		OverrideBaseDomain: authConfig.OverrideBaseDomain,
+		LogLevel:           logLevel,
+		Logger:             logger,
+		ClientID:           authConfig.ClientID,
+		ClientSecret:       authConfig.ClientSecret,
+	}
+
+	client, err := jamfpro.NewClient(config)
+	if err != nil {
+		log.Fatalf("Failed to create Jamf Pro client: %v", err)
+	}
+
+	resp, err := client.GetDefaultCloudIdentityProviderDefaultMappings("")
+
+}
