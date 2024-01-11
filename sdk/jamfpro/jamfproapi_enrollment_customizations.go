507da448f171efd0fa4d224b90dd6eb5f8cfc002
Joseph Little <joelittle11@live.com>
2024-01-11 10:00:08 +0000

    Enrollment customizations complete no examples

--- sdk/jamfpro/jamfproapi_enrollment_customizations.go
+++ sdk/jamfpro/jamfproapi_enrollment_customizations.go
@@ -15,8 +15,6 @@ const uriEnrollmentCustomizationSettings = "/api/v2/enrollment-customizations"
 
 // Structs
 
-// WIP
-
 // List
 
 type ResponseEnrollmentCustomizationList struct {
@@ -53,6 +51,10 @@ type EnrollmentCustomizationSubsetBrandingSettings struct {
 
 // CRUD
 
+// TODO Upload an image - https://developer.jamf.com/jamf-pro/reference/post_v2-enrollment-customizations-images
+// TODO Download an image - https://developer.jamf.com/jamf-pro/reference/get_v2-enrollment-customizations-images-id
+
+// Returns paginated list of Enrollment Customization
 func (c *Client) GetEnrollmentCustomizations(sort_filter string) (*ResponseEnrollmentCustomizationList, error) {
 	endpoint := uriEnrollmentCustomizationSettings
 	resp, err := c.DoPaginatedGet(
@@ -81,6 +83,7 @@ func (c *Client) GetEnrollmentCustomizations(sort_filter string) (*ResponseEnrol
 
 }
 
+// Returns single ResourceEnrollmentCustomization object matching given id
 func (c *Client) GetEnrollmentCustomizationByID(id string) (*ResourceEnrollmentCustomization, error) {
 	endpoint := fmt.Sprintf("%s/%s", uriEnrollmentCustomizationSettings, id)
 	var out ResourceEnrollmentCustomization
@@ -98,6 +101,7 @@ func (c *Client) GetEnrollmentCustomizationByID(id string) (*ResourceEnrollmentC
 	return &out, nil
 }
 
+// Creates new resource enrollment customization
 func (c *Client) CreateEnrollmentCustomization(enrollmentCustomization ResourceEnrollmentCustomization) (*ResponseEnrollmentCustomizationCreate, error) {
 	endpoint := uriEnrollmentCustomizationSettings
 	var out ResponseEnrollmentCustomizationCreate
@@ -113,4 +117,37 @@ func (c *Client) CreateEnrollmentCustomization(enrollmentCustomization ResourceE
 
 	return &out, nil
 
+}
+
+// Updates resource enrollment customization by id
+func (c *Client) UpdateEnrollmentCustomizationByID(id string, updatedCustomization ResourceEnrollmentCustomization) (*ResourceEnrollmentCustomization, error) {
+	endpoint := fmt.Sprintf("%s/%s", uriEnrollmentCustomizationSettings, id)
+	var out ResourceEnrollmentCustomization
+	resp, err := c.HTTP.DoRequest("PUT", endpoint, updatedCustomization, &out)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgFailedUpdateByID, "enrollment customization settings", id, err)
+	}
+
+	if resp != nil && resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	return &out, nil
+}
+
+// Deletes resource enrollment customization by id
+func (c *Client) DeleteEnrollmentCustomizationByID(id string) error {
+	endpoint := fmt.Sprintf("%s/%s", uriEnrollmentCustomizationSettings, id)
+
+	resp, err := c.HTTP.DoRequest("DELETE", endpoint, nil, nil)
+
+	if err != nil {
+		return fmt.Errorf(errMsgFailedDeleteByID, "enrollment customization settings", id, err)
+	}
+
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	return nil
 }
