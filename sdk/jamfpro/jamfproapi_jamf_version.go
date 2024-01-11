cedeff1e13714a244e1ba6d3d635eb6265b6fb1f
Joseph Little <joelittle11@live.com>
2024-01-11 10:26:56 +0000

    Jamf Protect, proserverurl endpoints done no examples

--- sdk/jamfpro/jamfproapi_jamf_version.go
+++
@@ -1,8 +0,0 @@
-// jamfproapi_jamf_version.go
-// Jamf Pro Api - Version
-// api reference: https://developer.jamf.com/jamf-pro/reference/get_v1-jamf-pro-version
-// Jamf Pro API requires the structs to support a JSON data structure.
-
-package jamfpro
-
-// TODO
