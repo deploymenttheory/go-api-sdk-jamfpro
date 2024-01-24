09a18c0f0efcfd7333bb0c2ed4f4f13a74e2384b
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-01-24 07:35:51 +0000

    alignment to main

--- sdk/jamfpro/jamfproapi_patch_software_title_configurations.go
+++ sdk/jamfpro/jamfproapi_patch_software_title_configurations.go
@@ -5,47 +5,48 @@
 
 package jamfpro
 
-import (
-	"fmt"
-)
+import "fmt"
 
-const uriPatchSoftwareTitleConfigurations = "/api/v2/patch-software-title-configurations"
+const uriPatchSoftwareTitleConfigurations = "/api/v2/patch-software-title-configurations/"
 
-// Response
+// Structs
 
-// Response format struct for create function
-type ResponsePatchSoftwareTitleConfigurationCreateAndUpdate struct {
+// List
+
+type ResponsePatchSoftwareTitleConfigurationList struct {
+	Results []ResourcePatchSoftwareTitleConfiguration
+}
+
+type ResponsePatchSoftwareTitleConfigurationCreate struct {
 	ID   string `json:"id"`
 	Href string `json:"href"`
 }
 
 // Resource
 
-// Struct which represents SoftwareTitleConfiguration object JSON from Pro API
 type ResourcePatchSoftwareTitleConfiguration struct {
-	DisplayName            string                                                `json:"displayName"`
-	CategoryId             string                                                `json:"categoryId"`
-	SiteId                 string                                                `json:"siteId"`
-	UINotifications        bool                                                  `json:"uiNotifications"`
-	EmailNotifications     bool                                                  `json:"emailNotifications"`
-	SoftwareTitleId        string                                                `json:"softwareTitleId"`
-	JamfOfficial           bool                                                  `json:"jamfOfficial"`
-	ExtensionAttributes    []SoftwareTitleConfigurationsSubsetExtensionAttribute `json:"extensionAttributes"`
-	SoftwareTitleName      string                                                `json:"softwareTitleName"`
-	SoftwareTitleNameId    string                                                `json:"softwareTitleNameId"`
-	SoftwareTitlePublisher string                                                `json:"softwareTitlePublisher"`
-	PatchSourceName        string                                                `json:"patchSourceName"`
-	PatchSourceEnabled     bool                                                  `json:"patchSourceEnabled"`
-	ID                     string                                                `json:"id"`
-	Packages               []SoftwareTitleConfigurationsSubsetPackage            `json:"packages"`
+	DisplayName            string                                                    `json:"displayName"`
+	CategoryID             string                                                    `json:"categoryId"`
+	SiteID                 string                                                    `json:"siteId"`
+	UiNotifications        bool                                                      `json:"uiNotifications"`
+	EmailNotifications     bool                                                      `json:"emailNotifications"`
+	SoftwareTitleID        string                                                    `json:"softwareTitleId"`
+	ExtensionAttributes    []PatchSoftwareTitleConfigurationSubsetExtensionAttribute `json:"extensionAttributes"`
+	SoftwareTitleName      string                                                    `json:"softwareTitleName"`
+	SoftwareTitleNameId    string                                                    `json:"softwareTitleNameId"`
+	SoftwareTitlePublisher string                                                    `json:"softwareTitlePublisher"`
+	ID                     string                                                    `json:"id"`
+	Packages               []PatchSoftwareTitleConfigurationSubsetPackage            `json:"packages"`
 }
 
-type SoftwareTitleConfigurationsSubsetExtensionAttribute struct {
+// Subsets
+
+type PatchSoftwareTitleConfigurationSubsetExtensionAttribute struct {
 	Accepted bool   `json:"accepted"`
-	EaId     string `json:"eaId"`
+	EaID     string `json:"eaId"`
 }
 
-type SoftwareTitleConfigurationsSubsetPackage struct {
+type PatchSoftwareTitleConfigurationSubsetPackage struct {
 	PackageId   string `json:"packageId"`
 	Version     string `json:"version"`
 	DisplayName string `json:"displayName"`
@@ -53,69 +54,82 @@ type SoftwareTitleConfigurationsSubsetPackage struct {
 
 // CRUD
 
-// GetPatchSoftwareTitleConfigurations retrieves a list of all Jamf App Catalog apps.
-func (c *Client) GetPatchSoftwareTitleConfigurations() ([]ResourcePatchSoftwareTitleConfiguration, error) {
+// GetPatchSoftwareTitleConfigurations retrieves list of PatchSoftwareTitleConfigurations
+func (c *Client) GetPatchSoftwareTitleConfigurations() (*ResponsePatchSoftwareTitleConfigurationList, error) {
 	endpoint := uriPatchSoftwareTitleConfigurations
-
-	var jamfAppCatalogApps []ResourcePatchSoftwareTitleConfiguration
-	resp, err := c.HTTP.DoRequest("GET", endpoint, nil, &jamfAppCatalogApps)
+	var out ResponsePatchSoftwareTitleConfigurationList
+	resp, err := c.HTTP.DoRequest("GET", endpoint, nil, &out)
 	if err != nil {
-		return nil, fmt.Errorf(errMsgFailedGet, "Jamf App Catalog apps", err)
-	}
-
-	if resp != nil && resp.Body != nil {
-		defer resp.Body.Close()
-	}
-
-	return jamfAppCatalogApps, nil
-}
-
-// Retrieves PatchSoftwareTitleConfiguration from provided ID & returns ResourcePatchSoftwareTitleConfiguration
-func (c *Client) GetPatchSoftwareTitleConfigurationByID(id string) (*ResourcePatchSoftwareTitleConfiguration, error) {
-	endpoint := fmt.Sprintf("%s/%s", uriPatchSoftwareTitleConfigurations, id)
-	var softwareTitleConfiguration ResourcePatchSoftwareTitleConfiguration
-	resp, err := c.HTTP.DoRequest("GET", endpoint, nil, &softwareTitleConfiguration)
-
-	if err != nil {
-		return nil, fmt.Errorf(errMsgFailedGetByID, "patch software title configuration", id, err)
-	}
-
-	if resp != nil && resp.Body != nil {
-		defer resp.Body.Close()
-	}
-
-	return &softwareTitleConfiguration, nil
-}
-
-// Retrieves PatchSoftwareTitleConfiguration by display Name by leveraging GetPatchSoftwareTitleConfigurations(), returns ResourcePatchSoftwareTitleConfiguration
-func (c *Client) GetPatchSoftwareTitleConfigurationByName(name string) (*ResourcePatchSoftwareTitleConfiguration, error) {
-	patchSoftwareTitleConfiguration, err := c.GetPatchSoftwareTitleConfigurations()
-	if err != nil {
-		return nil, fmt.Errorf(errMsgFailedPaginatedGet, "patch software title configuration", err)
-	}
-
-	for _, config := range patchSoftwareTitleConfiguration {
-		if config.DisplayName == name {
-			return &config, nil
-		}
-	}
-
-	return nil, fmt.Errorf(errMsgFailedGetByName, "patch software title configuration", name, errMsgNoName)
-}
-
-// Creates PatchSoftwareTitleConfiguration from ResourcePatchSoftwareTitleConfiguration struct
-func (c *Client) CreatePatchSoftwareTitleConfiguration(PatchSoftwareTitleConfiguration *ResourcePatchSoftwareTitleConfiguration) (*ResponsePatchSoftwareTitleConfigurationCreateAndUpdate, error) {
-	endpoint := uriPatchSoftwareTitleConfigurations
-	var ResponsePatchSoftwareTitleConfigurationCreate ResponsePatchSoftwareTitleConfigurationCreateAndUpdate
-
-	resp, err := c.HTTP.DoRequest("POST", endpoint, PatchSoftwareTitleConfiguration, &ResponsePatchSoftwareTitleConfigurationCreate)
-	if err != nil {
-		return nil, fmt.Errorf(errMsgFailedCreate, "Patch Software Title Configuration", err)
+		return nil, fmt.Errorf(errMsgFailedGet, "patch software title configurations", err)
 	}
 
 	if resp != nil {
 		defer resp.Body.Close()
 	}
 
-	return &ResponsePatchSoftwareTitleConfigurationCreate, nil
+	return &out, nil
+
+}
+
+// GetPatchSoftwareTitleConfigurationById retrieves a singular PatchSoftwareTitleConfiguration from a given ID
+func (c *Client) GetPatchSoftwareTitleConfigurationById(id string) (*ResourcePatchSoftwareTitleConfiguration, error) {
+	endpoint := fmt.Sprintf("%s/%s", uriPatchSoftwareTitleConfigurations, id)
+	var out ResourcePatchSoftwareTitleConfiguration
+	resp, err := c.HTTP.DoRequest("GET", endpoint, nil, &out)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgFailedGetByID, "patch software title configuration", id, err)
+	}
+
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	return &out, nil
+}
+
+// CreatePatchSoftwareTitleConfiguration Creates a new PatchSoftwareTitleConfiguration
+func (c *Client) CreatePatchSoftwareTitleConfiguration(configuration ResourcePatchSoftwareTitleConfiguration) (*ResponsePatchSoftwareTitleConfigurationCreate, error) {
+	endpoint := uriPatchSoftwareTitleConfigurations
+	var out ResponsePatchSoftwareTitleConfigurationCreate
+	resp, err := c.HTTP.DoRequest("POST", endpoint, configuration, &out)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgFailedCreate, "patch software title configuration", err)
+	}
+
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	return &out, nil
+}
+
+// UpdatePatchSoftwareTitleConfigurationById Updates a single PatchSoftwareTitleConfiguration with given ID
+func (c *Client) UpdatePatchSoftwareTitleConfigurationById(id string, updatedConfiguration ResourcePatchSoftwareTitleConfiguration) (*ResponsePatchSoftwareTitleConfigurationCreate, error) {
+	endpoint := fmt.Sprintf("%s/%s", uriPatchSoftwareTitleConfigurations, id)
+	var out ResponsePatchSoftwareTitleConfigurationCreate
+	resp, err := c.HTTP.DoRequest("POST", endpoint, updatedConfiguration, &out)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgFailedCreate, "patch software title configuration", err)
+	}
+
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	return &out, nil
+}
+
+// DeletePatchSoftwareTitleConfigurationById deletes a PatchSoftwareTitleConfiguration with given ID
+func (c *Client) DeletePatchSoftwareTitleConfigurationById(id string) error {
+	endpoint := fmt.Sprintf("%s/%s", uriPatchSoftwareTitleConfigurations, id)
+	resp, err := c.HTTP.DoRequest("DELETE", endpoint, nil, nil)
+	if err != nil {
+		return fmt.Errorf(errMsgFailedDeleteByID, "patch software title configuration", id, err)
+	}
+
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	return nil
 }
