64fec4a61a98aa69ac1ecfbd570bb1dfbe6c929f
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-01-24 07:37:31 +0000

    + example

--- examples/patch_software_title_configurations/CreatePatchSoftwareTitleConfiguration/CreatePatchSoftwareTitleConfiguration.go
+++ examples/patch_software_title_configurations/CreatePatchSoftwareTitleConfiguration/CreatePatchSoftwareTitleConfiguration.go
@@ -1,3 +1,66 @@
-// TODO
-
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/http_client"
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/GitHub/deploymenttheory/go-api-sdk-jamfpro/clientauth.json"
+
+	// Load the client OAuth credentials from the configuration file
+	authConfig, err := jamfpro.LoadClientAuthConfig(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to load client OAuth configuration: %v", err)
+	}
+
+	// Instantiate the default logger and set the desired log level
+	logger := http_client.NewDefaultLogger()
+	logLevel := http_client.LogLevelInfo // Adjust log level as needed
+
+	// Configuration for the jamfpro
+	config := jamfpro.Config{
+		InstanceName:       authConfig.InstanceName,
+		OverrideBaseDomain: authConfig.OverrideBaseDomain,
+		LogLevel:           logLevel,
+		Logger:             logger,
+		ClientID:           authConfig.ClientID,
+		ClientSecret:       authConfig.ClientSecret,
+	}
+
+	// Create a new jamfpro client instance
+	client, err := jamfpro.NewClient(config)
+	if err != nil {
+		log.Fatalf("Failed to create Jamf Pro client: %v", err)
+	}
+
+	// Create a new PatchSoftwareTitleConfiguration
+	newConfig := &jamfpro.ResourcePatchSoftwareTitleConfiguration{
+		DisplayName:        "CCleaner",
+		CategoryId:         "-1",
+		SiteId:             "-1",
+		UINotifications:    true,
+		EmailNotifications: true,
+		SoftwareTitleId:    "10",
+		ExtensionAttributes: []jamfpro.SoftwareTitleConfigurationsSubsetExtensionAttribute{
+			{
+				Accepted: true,
+				EaId:     "CCleaner-ea",
+			},
+		},
+	}
+
+	// Call the CreatePatchSoftwareTitleConfiguration method
+	response, err := client.CreatePatchSoftwareTitleConfiguration(newConfig)
+	if err != nil {
+		fmt.Println("Error creating Patch Software Title Configuration:", err)
+		return
+	}
+
+	// Print the response
+	fmt.Printf("Created Patch Software Title Configuration with ID: %s, Href: %s\n", response.ID, response.Href)
+}
