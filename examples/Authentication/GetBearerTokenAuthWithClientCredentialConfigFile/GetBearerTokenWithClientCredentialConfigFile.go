8acb27da52b91ca05dc596223c625fad5bd14315
Joseph Little <joelittle11@live.com>
2024-02-05 17:02:36 +0000

    Update authentication configuration in example files

--- examples/Authentication/GetBearerTokenAuthWithClientCredentialConfigFile/GetBearerTokenWithClientCredentialConfigFile.go
+++ examples/Authentication/GetBearerTokenAuthWithClientCredentialConfigFile/GetBearerTokenWithClientCredentialConfigFile.go
@@ -21,17 +21,20 @@ func main() {
 	fmt.Printf("Loaded Config: %+v\n", authConfig)
 
 	// Instantiate the default logger and set the desired log level
-	logger := http_client.NewDefaultLogger()
 	logLevel := http_client.LogLevelDebug // LogLevelNone // LogLevelWarning // LogLevelInfo  // LogLevelDebug
 
 	// Configuration for the jamfpro
 	config := http_client.Config{
+		InstanceName: authConfig.InstanceName,
+		Auth: http_client.AuthConfig{
+			Username: "fwfw",
+			Password: "fwfw",
+		},
 		LogLevel: logLevel,
-		Logger:   logger,
 	}
 
 	// Create a new client instance using the loaded InstanceName
-	client, err := http_client.NewClient(authConfig.InstanceName, config, nil)
+	client, err := http_client.NewClient(config)
 	if err != nil {
 		log.Fatalf("Failed to create new client: %v", err)
 	}
--- examples/Authentication/GetBearerTokenAuthWithUserCredentials/GetBearerTokenAuthWithUserCredentials.go
+++ examples/Authentication/GetBearerTokenAuthWithUserCredentials/GetBearerTokenAuthWithUserCredentials.go
@@ -11,20 +11,23 @@ func main() {
 	// Manually set your client credentials
 	username := "your-jamf-api-account"
 	password := "your-jamf-api-account-password"
-	baseURL := "your-jamf-instance" // e.g., "yourcompany.jamfcloud.com"
+	instanceName := "your-jamf-instance" // e.g., "yourcompany.jamfcloud.com"
 
 	// Instantiate the default logger and set the desired log level
-	logger := http_client.NewDefaultLogger()
 	logLevel := http_client.LogLevelDebug // LogLevelNone // LogLevelWarning // LogLevelInfo  // LogLevelDebug
 
 	// Configuration for the jamfpro
 	config := http_client.Config{
-		LogLevel: logLevel,
-		Logger:   logger,
+		InstanceName: instanceName,
+		LogLevel:     logLevel,
+		Auth: http_client.AuthConfig{
+			Username: username,
+			Password: password,
+		},
 	}
 
 	// Create a new client instance using the provided BaseURL
-	client, err := http_client.NewClient(baseURL, config, nil)
+	client, err := http_client.NewClient(config)
 	if err != nil {
 		log.Fatalf("Failed to create new client: %v", err)
 	}
