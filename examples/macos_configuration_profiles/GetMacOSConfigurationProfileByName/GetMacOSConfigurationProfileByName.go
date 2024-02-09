8a896de6aca146807ae93454f0d2fad093318096
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-02-09 17:13:25 +0000

    Refactor client configuration and remove unused logger import

--- examples/macos_configuration_profiles/GetMacOSConfigurationProfileByName/GetMacOSConfigurationProfileByName.go
+++ examples/macos_configuration_profiles/GetMacOSConfigurationProfileByName/GetMacOSConfigurationProfileByName.go
@@ -6,42 +6,48 @@ import (
 	"log"
 
 	"github.com/deploymenttheory/go-api-http-client/httpclient"
-	"github.com/deploymenttheory/go-api-http-client/logger"
 	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
 )
 
-const (
-	profileName = "accessibility"
-)
+const ()
 
 func main() {
-	// Define the path to the JSON configuration file inside the main function
+	// Define the path to the JSON configuration file
 	configFilePath := "/Users/dafyddwatkins/localtesting/clientauth.json"
 
 	// Load the client OAuth credentials from the configuration file
-	authConfig, err := jamfpro.LoadAuthConfig(configFilePath)
+	loadedConfig, err := jamfpro.LoadClientConfig(configFilePath)
 	if err != nil {
 		log.Fatalf("Failed to load client OAuth configuration: %v", err)
 	}
 
 	// Instantiate the default logger and set the desired log level
-	logLevel := logger.LogLevelDebug // LogLevelNone // LogLevelWarning // LogLevelInfo  // LogLevelDebug
+	//logLevel := logger.LogLevelInfo // LogLevelNone / LogLevelDebug / LogLevelInfo / LogLevelError
 
-	// Configuration for the jamfpro
-	config := httpclient.Config{
-		InstanceName: authConfig.InstanceName,
+	// Configuration for the HTTP client
+	config := httpclient.ClientConfig{
 		Auth: httpclient.AuthConfig{
-			ClientID:     authConfig.ClientID,
-			ClientSecret: authConfig.ClientSecret,
+			ClientID:     loadedConfig.Auth.ClientID,
+			ClientSecret: loadedConfig.Auth.ClientSecret,
 		},
-		LogLevel: logLevel,
+		Environment: httpclient.EnvironmentConfig{
+			APIType:      loadedConfig.Environment.APIType,
+			InstanceName: loadedConfig.Environment.InstanceName,
+		},
+		ClientOptions: httpclient.ClientOptions{
+			LogLevel:          loadedConfig.ClientOptions.LogLevel,
+			HideSensitiveData: loadedConfig.ClientOptions.HideSensitiveData,
+			LogOutputFormat:   loadedConfig.ClientOptions.LogOutputFormat,
+		},
 	}
 
-	// Create a new jamfpro client instanceclient,
+	// Create a new jamfpro client instance
 	client, err := jamfpro.BuildClient(config)
 	if err != nil {
 		log.Fatalf("Failed to create Jamf Pro client: %v", err)
 	}
+
+	profileName := "accessibility"
 
 	// Call GetMacOSConfigurationProfileByName function
 	profile, err := client.GetMacOSConfigurationProfileByName(profileName)
