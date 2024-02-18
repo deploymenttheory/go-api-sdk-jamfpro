24c35aeb2ffcdaf80eb78af507890f64997a378e
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-02-18 18:22:49 +0000

    Update code formatting and fix bug in file processing

---
+++ examples/packages/DeletePackageByID/DeletePackageByID.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-http-client/httpclient"
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/clientconfig.json"
+	// Load the client OAuth credentials from the configuration file
+	loadedConfig, err := jamfpro.LoadClientConfig(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to load client OAuth configuration: %v", err)
+	}
+
+	// Configuration for the HTTP client
+	config := httpclient.ClientConfig{
+		Auth: httpclient.AuthConfig{
+			ClientID:     loadedConfig.Auth.ClientID,
+			ClientSecret: loadedConfig.Auth.ClientSecret,
+		},
+		Environment: httpclient.EnvironmentConfig{
+			APIType:      loadedConfig.Environment.APIType,
+			InstanceName: loadedConfig.Environment.InstanceName,
+		},
+		ClientOptions: httpclient.ClientOptions{
+			LogLevel:          loadedConfig.ClientOptions.LogLevel,
+			HideSensitiveData: loadedConfig.ClientOptions.HideSensitiveData,
+			LogOutputFormat:   loadedConfig.ClientOptions.LogOutputFormat,
+		},
+	}
+
+	// Create a new jamfpro client instance
+	client, err := jamfpro.BuildClient(config)
+	if err != nil {
+		log.Fatalf("Failed to create Jamf Pro client: %v", err)
+	}
+
+	packageID := 1
+
+	err = client.DeletePackageByID(packageID)
+	if err != nil {
+		log.Fatalf("Error deleting network package by ID: %v", err)
+	} else {
+		fmt.Printf("Network package with ID %d successfully deleted.\n", packageID)
+	}
+}
---
+++ examples/packages/UpdatePackageByID/UpdatePackageByID.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-http-client/httpclient"
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/clientconfig.json"
+	// Load the client OAuth credentials from the configuration file
+	loadedConfig, err := jamfpro.LoadClientConfig(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to load client OAuth configuration: %v", err)
+	}
+
+	// Instantiate the default logger and set the desired log level
+	//logLevel := logger.LogLevelInfo // LogLevelNone / LogLevelDebug / LogLevelInfo / LogLevelError
+
+	// Configuration for the HTTP client
+	config := httpclient.ClientConfig{
+		Auth: httpclient.AuthConfig{
+			ClientID:     loadedConfig.Auth.ClientID,
+			ClientSecret: loadedConfig.Auth.ClientSecret,
+		},
+		Environment: httpclient.EnvironmentConfig{
+			APIType:      loadedConfig.Environment.APIType,
+			InstanceName: loadedConfig.Environment.InstanceName,
+		},
+		ClientOptions: httpclient.ClientOptions{
+			LogLevel:          loadedConfig.ClientOptions.LogLevel,
+			HideSensitiveData: loadedConfig.ClientOptions.HideSensitiveData,
+			LogOutputFormat:   loadedConfig.ClientOptions.LogOutputFormat,
+		},
+	}
+
+	// Create a new jamfpro client instance
+	client, err := jamfpro.BuildClient(config)
+	if err != nil {
+		log.Fatalf("Failed to create Jamf Pro client: %v", err)
+	}
+
+	// Define the package update
+	updatedPackage := &jamfpro.ResourcePackage{
+		Name:     "Updated Package Name",
+		Category: "Productivity",
+		Filename: "updated_package.zip",
+		Info:     "This is an updated package for office productivity tools.",
+		Notes:    "Please ensure compatibility before deploying.",
+		Priority: 10,
+	}
+
+	packageID := 1 // Replace with actual ID
+
+	// Update the package by ID
+	updated, err := client.UpdatePackageByID(packageID, updatedPackage)
+	if err != nil {
+		log.Fatalf("Error updating package by ID: %v", err)
+	}
+
+	// Print the updated package details
+	packageXML, _ := xml.MarshalIndent(updated, "", "    ")
+	fmt.Println("Updated Package:", string(packageXML))
+}
