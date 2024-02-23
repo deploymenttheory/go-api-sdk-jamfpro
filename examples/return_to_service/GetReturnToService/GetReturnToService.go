2f8b70745e34a511cbe1090dbf51404160de633c
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-02-23 17:01:35 +0000

    Add GetJCDS2Properties function to fetch file list from Jamf Cloud Distribution Service

---
+++ examples/return_to_service/GetReturnToService/GetReturnToService.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-http-client/httpclient"
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
+	// Load the client OAuth credentials from the configuration file
+	loadedConfig, err := jamfpro.LoadClientConfig(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to load client OAuth configuration: %v", err)
+	}
+
+	// Configuration for the HTTP client
+	config := httpclient.ClientConfig{
+		Auth: httpclient.AuthConfig{
+			ClientID:     loadedConfig.Auth.ClientID,
+			ClientSecret: loadedConfig.Auth.ClientSecret,
+		},
+		Environment: httpclient.EnvironmentConfig{
+			APIType:      loadedConfig.Environment.APIType,
+			InstanceName: loadedConfig.Environment.InstanceName,
+		},
+		ClientOptions: httpclient.ClientOptions{
+			LogLevel:            loadedConfig.ClientOptions.LogLevel,
+			LogOutputFormat:     loadedConfig.ClientOptions.LogOutputFormat,
+			LogConsoleSeparator: loadedConfig.ClientOptions.LogConsoleSeparator,
+			HideSensitiveData:   loadedConfig.ClientOptions.HideSensitiveData,
+		},
+	}
+
+	// Create a new jamfpro client instance
+	client, err := jamfpro.BuildClient(config)
+	if err != nil {
+		log.Fatalf("Failed to create Jamf Pro client: %v", err)
+	}
+
+	// Call GetReturnToService function
+	returnToService, err := client.GetReturnToService()
+	if err != nil {
+		log.Fatalf("Error fetching return to service  properties: %v", err)
+	}
+
+	// Pretty print the return to service  files in JSON
+	response, err := json.MarshalIndent(returnToService, "", "    ") // Indent with 4 spaces
+	if err != nil {
+		log.Fatalf("Error marshaling return to service data: %v", err)
+	}
+	fmt.Println("Fetched return to service properties:\n", string(response))
+}
