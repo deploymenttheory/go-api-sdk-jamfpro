d7c467c645682802858d5e758fe2bc47c7c67059
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-03-12 08:38:17 +0000

    Refactor code to improve performance and readability

---
+++ examples/jcds2/DeleteJCDS2PackageByFileName/DeleteJCDS2PackageByFileName.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
+
+	// Initialize the Jamf Pro client with the HTTP client configuration
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	// Define the name of the macOS Configuration Profile you want to delete
+	packageFileName := "microsoft-edge-121-0-2277-106.pkg"
+
+	// Call the DeleteMacApplicationByName function
+	err = client.DeleteJCDS2PackageV2(packageFileName)
+	if err != nil {
+		log.Fatalf("Failed to delete JCDS 2.0 file with name '%s': %v", packageFileName, err)
+	}
+
+	fmt.Printf("JCDS 2.0 file with name '%s' deleted successfully\n", packageFileName)
+}
