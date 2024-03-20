d1aad26d79693d7c4b8c875f0b5ffc004907e78a
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-03-20 11:57:05 +0000

    Refactor resource availability check to use resource ID in URL

--- sdk/jamfpro/util_ping_jamf_resources.go
+++ sdk/jamfpro/util_ping_jamf_resources.go
@@ -6,23 +6,26 @@ import (
 	"time"
 )
 
-// WaitForResourceAvailable checks if a resource is available by making a GET request to the resource's URL.
+// PingJamfResource checks if a resource is available by making a GET request to the resource's URL, using the provided resourceId.
 // It retries the request with exponential backoff until a 200 OK response is received or the max wait time is reached.
-// resourceURL: The URL to check for resource availability.
+// resourceURL: The base URL to check for resource availability.
+// resourceId: The unique identifier of the resource to be checked.
 // maxWaitTime: Maximum time to wait for the resource to become available.
-// client: The HTTP client used to make the request. Ensure it's properly authenticated to access the resource.
-func (c *Client) WaitForResourceAvailable(resourceURL string, maxWaitTime time.Duration) error {
+func (c *Client) PingJamfResource(resourceURL string, resourceId string, maxWaitTime time.Duration) error {
 	var backoffTime = 1 * time.Second
 	const backoffFactor = 2
 	const maxBackoffTime = 30 * time.Second
+
+	// Construct the full URL with the resource ID
+	fullURL := fmt.Sprintf("%s/id/%s", resourceURL, resourceId)
 
 	startTime := time.Now()
 
 	for {
 		// Make a request to check resource availability
-		resp, err := c.HTTP.DoRequest("GET", resourceURL, nil, nil)
+		resp, err := c.HTTP.DoRequest("GET", fullURL, nil, nil)
 		if err != nil {
-			return fmt.Errorf("error making request to check resource availability: %v", err)
+			return fmt.Errorf("error making request to check resource availability at %s: %v", fullURL, err)
 		}
 
 		if resp.StatusCode == http.StatusOK {
@@ -32,7 +35,7 @@ func (c *Client) WaitForResourceAvailable(resourceURL string, maxWaitTime time.D
 
 		if time.Since(startTime) > maxWaitTime {
 			// Max wait time exceeded
-			return fmt.Errorf("timed out waiting for resource to become available")
+			return fmt.Errorf("timed out waiting for resource to become available at %s", fullURL)
 		}
 
 		// Wait for backoff time before retrying
