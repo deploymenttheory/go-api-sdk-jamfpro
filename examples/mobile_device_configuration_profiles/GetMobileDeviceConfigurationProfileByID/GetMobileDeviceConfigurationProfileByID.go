71cca56cc9e8f748aae097bad3f4d5cbd3f7a752
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-04-11 17:17:29 +0100

    Update profileID in GetMobileDeviceConfigurationProfileByID.go

--- examples/mobile_device_configuration_profiles/GetMobileDeviceConfigurationProfileByID/GetMobileDeviceConfigurationProfileByID.go
+++ examples/mobile_device_configuration_profiles/GetMobileDeviceConfigurationProfileByID/GetMobileDeviceConfigurationProfileByID.go
@@ -17,7 +17,7 @@ func main() {
 		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
 	}
 
-	profileID := 123 // Replace with the actual ID
+	profileID := 5 // Replace with the actual ID
 	profile, err := client.GetMobileDeviceConfigurationProfileByID(profileID)
 	if err != nil {
 		log.Fatalf("Error fetching mobile device configuration profile by ID: %v", err)
