52f54eb66d03e52c48921ab6054aaef92fc88be8
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-05-28 08:16:23 +0100

    modified:   examples/macos_configuration_profiles/GetMacOSConfigurationProfileByID/GetMacOSConfigurationProfileByID.go

--- examples/macos_configuration_profiles/GetMacOSConfigurationProfileByID/GetMacOSConfigurationProfileByID.go
+++ examples/macos_configuration_profiles/GetMacOSConfigurationProfileByID/GetMacOSConfigurationProfileByID.go
@@ -18,7 +18,7 @@ func main() {
 		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
 	}
 
-	profileID := 2968
+	profileID := 3062
 
 	// Call GetMacOSConfigurationProfileByID function
 	profile, err := client.GetMacOSConfigurationProfileByID(profileID)
