e2df74261709c50271728ecbcb3041b653ff2090
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-05-28 13:25:04 +0100

    chore: Update go.mod with new indirect dependencies

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 )
 
 require (
+	github.com/ahmetb/go-linq v3.0.0+incompatible // indirect
 	github.com/antchfx/xmlquery v1.4.0 // indirect
 	github.com/antchfx/xpath v1.3.0 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.2 // indirect
@@ -30,12 +31,28 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.24.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.28.9 // indirect
 	github.com/aws/smithy-go v1.20.2 // indirect
+	github.com/dave/jennifer v1.4.1 // indirect
+	github.com/getkin/kin-openapi v0.75.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/swag v0.19.5 // indirect
+	github.com/goioc/di v1.5.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/heetch/confita v0.9.2 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
+	github.com/mikekonan/go-oas3 v1.0.53 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/tdewolff/minify/v2 v2.9.11 // indirect
+	github.com/tdewolff/parse/v2 v2.5.8 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- tools/schema/helpers/schemaprocessing/schemaprocessing.go
+++ tools/schema/helpers/schemaprocessing/schemaprocessing.go
@@ -7,6 +7,7 @@ import (
 	"html/template"
 	"io"
 	"os"
+	"regexp"
 	"sort"
 	"strings"
 
@@ -47,64 +48,192 @@ func ParseJSONSchema(schema []byte) (string, error) {
 		return "", fmt.Errorf("failed to unmarshal JSON schema: %w", err)
 	}
 
-	structs, err := generateStructs("Root", schemaData)
+	structs, err := generateRootStructs(schemaData)
 	if err != nil {
-		return "", fmt.Errorf("failed to generate structs: %w", err)
+		return "", fmt.Errorf("failed to generate root structs: %w", err)
 	}
 
 	return structs, nil
 }
 
-// generateStructs generates Go struct definitions from the JSON schema
-func generateStructs(structName string, schemaData map[string]interface{}) (string, error) {
+// generateRootStructs generates Go struct definitions for the root-level fields of the OpenAPI schema
+func generateRootStructs(schemaData map[string]interface{}) (string, error) {
 	var structsBuilder strings.Builder
 	structsBuilder.WriteString("package generatedstructs\n\n")
 
-	structDef, err := generateStruct(structName, schemaData)
-	if err != nil {
+	// Generate the OpenAPI root struct
+	openAPIFields := []string{
+		"Openapi string `json:\"openapi\"`",
+		"Servers []Server `json:\"servers\"`",
+		"Security []map[string]interface{} `json:\"security\"`",
+		"Paths Paths `json:\"paths\"`",
+		"Components Components `json:\"components\"`",
+	}
+
+	structsBuilder.WriteString("type OpenAPI struct {\n")
+	for _, field := range openAPIFields {
+		structsBuilder.WriteString(field + "\n")
+	}
+	structsBuilder.WriteString("}\n\n")
+
+	// Generate structs for the individual top-level fields
+	if err := generateServersStruct(&structsBuilder, schemaData); err != nil {
 		return "", err
 	}
-	structsBuilder.WriteString(structDef)
-	structsBuilder.WriteString("\n\n")
 
-	keys := make([]string, 0, len(schemaData))
-	for key := range schemaData {
-		keys = append(keys, key)
+	if err := generatePathsStruct(&structsBuilder, schemaData); err != nil {
+		return "", err
 	}
-	sort.Strings(keys)
 
-	for _, key := range keys {
-		value := schemaData[key]
-		switch value := value.(type) {
-		case map[string]interface{}:
-			structDef, err := generateStruct(cases.Title(language.English).String(key), value)
-			if err != nil {
-				return "", err
-			}
-			structsBuilder.WriteString(structDef)
-			structsBuilder.WriteString("\n\n")
-		case []interface{}:
-			if len(value) > 0 {
-				if elem, ok := value[0].(map[string]interface{}); ok {
-					structDef, err := generateStruct(cases.Title(language.English).String(key), elem)
-					if err != nil {
-						return "", err
-					}
-					structsBuilder.WriteString(structDef)
-					structsBuilder.WriteString("\n\n")
-				}
-			}
-		default:
-			fieldType, err := getFieldType(value)
-			if err != nil {
-				return "", err
-			}
-			fieldName := cases.Title(language.English).String(key)
-			structsBuilder.WriteString(fmt.Sprintf("%s %s `json:\"%s\" xml:\"%s\"`\n", fieldName, fieldType, key, key))
-		}
+	if err := generateComponentsStruct(&structsBuilder, schemaData); err != nil {
+		return "", err
 	}
 
 	return structsBuilder.String(), nil
+}
+
+// generateServersStruct generates the Go struct for the "servers" field
+func generateServersStruct(structsBuilder *strings.Builder, schemaData map[string]interface{}) error {
+	servers, ok := schemaData["servers"].([]interface{})
+	if !ok {
+		return nil
+	}
+
+	for _, server := range servers {
+		serverMap, ok := server.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		serverStruct, err := generateStruct("Server", serverMap)
+		if err != nil {
+			return err
+		}
+
+		structsBuilder.WriteString(serverStruct)
+		structsBuilder.WriteString("\n\n")
+	}
+
+	return nil
+}
+
+// generatePathsStruct generates the Go struct for the "paths" field
+func generatePathsStruct(structsBuilder *strings.Builder, schemaData map[string]interface{}) error {
+	paths, ok := schemaData["paths"].(map[string]interface{})
+	if !ok {
+		return nil
+	}
+
+	taggedPaths := make(map[string]map[string]interface{})
+
+	for path, pathItem := range paths {
+		pathItemMap, ok := pathItem.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		for method, operation := range pathItemMap {
+			operationMap, ok := operation.(map[string]interface{})
+			if !ok {
+				continue
+			}
+
+			if tags, ok := operationMap["tags"].([]interface{}); ok {
+				for _, tag := range tags {
+					tagStr, ok := tag.(string)
+					if !ok {
+						continue
+					}
+
+					if _, exists := taggedPaths[tagStr]; !exists {
+						taggedPaths[tagStr] = make(map[string]interface{})
+					}
+
+					sanitizedPath := sanitizeFieldName(path)
+					taggedPaths[tagStr][method+"_"+sanitizedPath] = operationMap
+				}
+			}
+		}
+	}
+
+	titleCaser := cases.Title(language.English)
+
+	// Create top-level Paths struct
+	structsBuilder.WriteString("type Paths struct {\n")
+	tagStructNames := make([]string, 0, len(taggedPaths))
+
+	for tag := range taggedPaths {
+		tagStructName := sanitizeFieldName(tag)
+		tagStructNames = append(tagStructNames, tagStructName)
+	}
+	sort.Strings(tagStructNames)
+
+	for _, tagStructName := range tagStructNames {
+		structsBuilder.WriteString(fmt.Sprintf("%s %s `json:\"%s\"`\n", titleCaser.String(tagStructName), titleCaser.String(tagStructName), tagStructName))
+	}
+	structsBuilder.WriteString("}\n\n")
+
+	// Generate structs for each tag
+	for _, tag := range tagStructNames {
+		paths := taggedPaths[tag]
+		tagStructName := sanitizeFieldName(tag)
+		structsBuilder.WriteString(fmt.Sprintf("type %s struct {\n", titleCaser.String(tagStructName)))
+
+		methodPaths := make([]string, 0, len(paths))
+		for methodPath := range paths {
+			methodPaths = append(methodPaths, methodPath)
+		}
+		sort.Strings(methodPaths)
+
+		for _, methodPath := range methodPaths {
+			structName := sanitizeFieldName(methodPath)
+			fieldName := titleCaser.String(structName)
+			structsBuilder.WriteString(fmt.Sprintf("%s %s `json:\"%s\"`\n", fieldName, structName, methodPath))
+		}
+
+		structsBuilder.WriteString("}\n\n")
+
+		for _, methodPath := range methodPaths {
+			structName := sanitizeFieldName(methodPath)
+			pathItem := paths[methodPath]
+			pathItemStruct, err := generateStruct(structName, pathItem.(map[string]interface{}))
+			if err != nil {
+				return err
+			}
+			structsBuilder.WriteString(pathItemStruct)
+			structsBuilder.WriteString("\n\n")
+		}
+	}
+
+	return nil
+}
+
+// generateComponentsStruct generates the Go struct for the "components" field
+func generateComponentsStruct(structsBuilder *strings.Builder, schemaData map[string]interface{}) error {
+	components, ok := schemaData["components"].(map[string]interface{})
+	if !ok {
+		return nil
+	}
+
+	componentStruct, err := generateStruct("Components", components)
+	if err != nil {
+		return err
+	}
+	structsBuilder.WriteString(componentStruct)
+	structsBuilder.WriteString("\n\n")
+
+	for key, value := range components {
+		if nestedProps, ok := value.(map[string]interface{}); ok {
+			nestedStructDef, err := generateStruct(cases.Title(language.English).String(key), nestedProps)
+			if err != nil {
+				return err
+			}
+			structsBuilder.WriteString(nestedStructDef)
+			structsBuilder.WriteString("\n\n")
+		}
+	}
+
+	return nil
 }
 
 // generateStruct generates a Go struct definition from a JSON object
@@ -113,12 +242,12 @@ func generateStruct(structName string, structData map[string]interface{}) (strin
 	titleCaser := cases.Title(language.English)
 
 	for key, value := range structData {
-		fieldType, err := getFieldType(value)
+		fieldType, err := getFieldType(key, value)
 		if err != nil {
 			return "", err
 		}
 
-		fieldName := titleCaser.String(key)
+		fieldName := sanitizeFieldName(titleCaser.String(key))
 		fieldsBuilder.WriteString(fmt.Sprintf("%s %s `json:\"%s\" xml:\"%s\"`\n", fieldName, fieldType, key, key))
 	}
 
@@ -144,13 +273,16 @@ func generateStruct(structName string, structData map[string]interface{}) (strin
 }
 
 // getFieldType returns the Go type of a field based on its value
-func getFieldType(value interface{}) (string, error) {
+func getFieldType(fieldName string, value interface{}) (string, error) {
 	switch v := value.(type) {
 	case map[string]interface{}:
-		return "struct", nil
+		if _, ok := v["properties"]; ok {
+			return cases.Title(language.English).String(fieldName), nil
+		}
+		return "map[string]interface{}", nil
 	case []interface{}:
 		if len(v) > 0 {
-			elemType, err := getFieldType(v[0])
+			elemType, err := getFieldType(fieldName, v[0])
 			if err != nil {
 				return "", err
 			}
@@ -168,4 +300,11 @@ func getFieldType(value interface{}) (string, error) {
 	default:
 		return "", fmt.Errorf("unknown field type: %T", v)
 	}
+}
+
+// sanitizeFieldName removes invalid characters in struct field names
+func sanitizeFieldName(fieldName string) string {
+	// Remove invalid characters, specifically '/'
+	reg := regexp.MustCompile(`[/]`)
+	return reg.ReplaceAllString(fieldName, "")
 }
