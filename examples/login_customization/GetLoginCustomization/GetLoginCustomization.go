7b513817a0573dfb9845cc62454f455615ac1c89
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-06-03 10:49:59 +0100

    feat: added login customization with examples to sdk

---
+++ examples/login_customization/GetLoginCustomization/GetLoginCustomization.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
+
+	// Initialize the Jamf Pro client with the HTTP client configuration
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	loginCustomization, err := client.GetLoginCustomization()
+	if err != nil {
+		log.Fatalf("Error fetching Mac Applications: %v", err)
+	}
+
+	// Pretty print the login customization details
+	responseJSON, err := json.MarshalIndent(loginCustomization, "", "    ") // Indent with 4 spaces
+	if err != nil {
+		log.Fatalf("Error marshaling login customization data: %v", err)
+	}
+	fmt.Println("Fetched login customization Details:", string(responseJSON))
+}
---
+++ examples/login_customization/UpdateLoginCustomization/UpdateLoginCustomization.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
+
+	// Initialize the Jamf Pro client with the HTTP client configuration
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	// Define new login customization settings
+	newLoginCustomization := &jamfpro.ResourceLoginCustomization{
+		RampInstance:            true,
+		IncludeCustomDisclaimer: true,
+		DisclaimerHeading:       "Updated Disclaimer Header",
+		DisclaimerMainText:      "Updated disclaimer main text",
+		ActionText:              "Accept",
+	}
+
+	// Update the login customization settings
+	updatedLoginCustomization, err := client.UpdateLoginCustomization(newLoginCustomization)
+	if err != nil {
+		log.Fatalf("Error updating login customization: %v", err)
+	}
+
+	// Pretty print the updated login customization settings
+	updatedResponseJSON, err := json.MarshalIndent(updatedLoginCustomization, "", "    ") // Indent with 4 spaces
+	if err != nil {
+		log.Fatalf("Error marshaling updated login customization data: %v", err)
+	}
+	fmt.Println("Updated login customization details:", string(updatedResponseJSON))
+}
