d6f9ef39441d8628b292c15509e380bd9d65e806
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-09-08 21:23:46 +0100

    Added force DDM sync function

---
+++ examples/declarative_device_management/ForceDDMSync/ForceDDMSync.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
+
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	// Define the client management ID for which to force DDM sync
+	clientManagementID := "10" // Replace with the actual client management ID
+
+	// Call ForceDDMSync function
+	err = client.ForceDDMSync(clientManagementID)
+	if err != nil {
+		log.Fatalf("Error forcing DDM sync: %v", err)
+	}
+
+	fmt.Printf("Successfully forced DDM sync for client management ID: %s\n", clientManagementID)
+}
