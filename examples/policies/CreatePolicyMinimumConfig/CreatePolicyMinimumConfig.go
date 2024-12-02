0b3d36c77dedfa622f5371c8eace7b96d0fc50a2
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-12-02 11:19:20 +0000

    Update CreatePolicyMinimumConfig to define a new policy with minimum viable configuration and boolean fields for clarity

--- examples/policies/CreatePolicyMinimumConfig/CreatePolicyMinimumConfig.go
+++ examples/policies/CreatePolicyMinimumConfig/CreatePolicyMinimumConfig.go
@@ -9,19 +9,94 @@ import (
 )
 
 func main() {
-	// Define the path to the JSON configuration file
 	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
 
-	// Initialize the Jamf Pro client with the HTTP client configuration
 	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
 	if err != nil {
 		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
 	}
 
-	// Define a new policy with all required fields
 	newPolicy := &jamfpro.ResourcePolicy{
 		General: jamfpro.PolicySubsetGeneral{
-			Name: "jamfpro-sdk-minimum-policy-config",
+			Name:                       "example-policy-with-minimum-viable-config",
+			Enabled:                    false,
+			TriggerCheckin:             false,
+			TriggerEnrollmentComplete:  false,
+			TriggerLogin:               false,
+			TriggerLogout:              false,
+			TriggerNetworkStateChanged: false,
+			TriggerStartup:             false,
+			TriggerOther:               "EVENT",
+			Frequency:                  "Once per computer",
+			RetryEvent:                 "none",
+			RetryAttempts:              -1,
+			NotifyOnEachFailedRetry:    false,
+			LocationUserOnly:           false,
+			TargetDrive:                "/",
+			Offline:                    false,
+			Category: &jamfpro.SharedResourceCategory{
+				ID:   -1,
+				Name: "No category assigned",
+			},
+			NetworkLimitations: &jamfpro.PolicySubsetGeneralNetworkLimitations{
+				MinimumNetworkConnection: "No Minimum",
+				AnyIPAddress:             false,
+				NetworkSegments:          "",
+			},
+			NetworkRequirements: "Any",
+			Site: &jamfpro.SharedResourceSite{
+				ID:   -1,
+				Name: "NONE",
+			},
+		},
+		Scope: jamfpro.PolicySubsetScope{
+			AllComputers: false,
+			AllJSSUsers:  false,
+		},
+		SelfService: jamfpro.PolicySubsetSelfService{
+			UseForSelfService:           true,
+			InstallButtonText:           "Install",
+			ReinstallButtonText:         "REINSTALL",
+			ForceUsersToViewDescription: false,
+			FeatureOnMainPage:           false,
+			Notification:                false,
+		},
+		Scripts: []jamfpro.PolicySubsetScript{},
+		Printers: jamfpro.PolicySubsetPrinters{
+			LeaveExistingDefault: false,
+		},
+		Maintenance: jamfpro.PolicySubsetMaintenance{
+			Recon:                    false,
+			ResetName:                false,
+			InstallAllCachedPackages: false,
+			Heal:                     false,
+			Prebindings:              false,
+			Permissions:              false,
+			Byhost:                   false,
+			SystemCache:              false,
+			UserCache:                false,
+			Verify:                   false,
+		},
+		FilesProcesses: jamfpro.PolicySubsetFilesProcesses{
+			DeleteFile:           false,
+			UpdateLocateDatabase: false,
+			KillProcess:          false,
+		},
+		UserInteraction: jamfpro.PolicySubsetUserInteraction{
+			AllowUsersToDefer:    false,
+			AllowDeferralMinutes: 0,
+		},
+		DiskEncryption: jamfpro.PolicySubsetDiskEncryption{
+			Action:                        "",
+			DiskEncryptionConfigurationID: 0,
+			AuthRestart:                   false,
+			RemediateKeyType:              "Individual",
+		},
+		Reboot: jamfpro.PolicySubsetReboot{
+			StartupDisk:                 "Current Startup Disk",
+			MinutesUntilReboot:          0,
+			StartRebootTimerImmediately: false,
+			FileVault2Reboot:            false,
 		},
 	}
 
@@ -31,14 +106,12 @@ func main() {
 	}
 	fmt.Println("Policy Details to be Sent:\n", string(policyXML))
 
-	// Call CreatePolicy function
 	createdPolicy, err := client.CreatePolicy(newPolicy)
 	if err != nil {
 		log.Fatalf("Error creating policy: %v", err)
 	}
 
-	// Pretty print the created policy details in XML
-	policyXML, err = xml.MarshalIndent(createdPolicy, "", "    ") // Indent with 4 spaces and use '='
+	policyXML, err = xml.MarshalIndent(createdPolicy, "", "    ")
 	if err != nil {
 		log.Fatalf("Error marshaling policy details data: %v", err)
 	}
