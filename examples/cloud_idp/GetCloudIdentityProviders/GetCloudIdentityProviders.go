c37cd0eb4ef7a38d8adbce3d83b180731af321f5
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-12-11 12:27:18 +0000

    Feat: add examples for retrieving cloud identity provider configurations and startup status

---
+++ examples/cloud_idp/GetCloudIdentityProviders/GetCloudIdentityProviders.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
+
+	// Initialize the Jamf Pro client with the HTTP client configuration
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	// Optional: Define sort filter (e.g., "id:asc" or "displayName:desc")
+	sortFilter := "id:desc"
+
+	// Call GetCloudIdentityProviders function
+	cloudIdps, err := client.GetCloudIdentityProviders(sortFilter)
+	if err != nil {
+		log.Fatalf("Error fetching cloud identity providers: %v", err)
+	}
+
+	// Pretty print the cloud identity providers in JSON
+	JSON, err := json.MarshalIndent(cloudIdps, "", "    ") // Indent with 4 spaces
+	if err != nil {
+		log.Fatalf("Error marshaling cloud identity providers data: %v", err)
+	}
+	fmt.Println("Cloud Identity Providers:\n", string(JSON))
+}
---
+++ examples/startup_status/GetStartupStatus/GetStartupStatus.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
+
+	// Initialize the Jamf Pro client with the HTTP client configuration
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	// Call GetStartupStatus function
+	startupStatus, err := client.GetStartupStatus()
+	if err != nil {
+		log.Fatalf("Error fetching startup status: %v", err)
+	}
+
+	// Pretty print the startup status in JSON
+	JSON, err := json.MarshalIndent(startupStatus, "", "    ") // Indent with 4 spaces
+	if err != nil {
+		log.Fatalf("Error marshaling startup status data: %v", err)
+	}
+	fmt.Println("Jamf Pro Startup Status:\n", string(JSON))
+}
