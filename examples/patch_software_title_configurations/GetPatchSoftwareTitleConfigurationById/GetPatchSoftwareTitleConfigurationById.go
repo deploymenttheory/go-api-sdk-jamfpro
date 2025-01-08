65f4ee06bbc94d6b0da3da5e376b385a5e192b77
ShocOne <62835948+ShocOne@users.noreply.github.com>
2025-01-08 16:07:10 +0000

    feat: update GetPatchPoliciesByID to return JSON and add GetPatchSoftwareTitleConfigurationById example

---
+++ examples/patch_software_title_configurations/GetPatchSoftwareTitleConfigurationById/GetPatchSoftwareTitleConfigurationById.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
+
+	// Initialize the Jamf Pro client with the HTTP client configuration
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	// Example display name
+	patchSoftwareTitleConfigurationId := "2" // Replace with an actual device name
+
+	// Get patch software title configuration by name
+	response, err := client.GetPatchSoftwareTitleConfigurationById(patchSoftwareTitleConfigurationId)
+	if err != nil {
+		log.Fatalf("Error fetching patch software title configuration by name: %v", err)
+	}
+
+	// Pretty print the network segments in XML
+	mobileDeviceXML, err := xml.MarshalIndent(response, "", "    ") // Indent with 4 spaces
+	if err != nil {
+		log.Fatalf("Error marshaling network segments data: %v", err)
+	}
+	fmt.Println("Network Segments:\n", string(mobileDeviceXML))
+}
