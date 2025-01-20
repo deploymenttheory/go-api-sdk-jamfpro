a9785e57b2fed29795409f934cdc428b7c43640e
ShocOne <62835948+ShocOne@users.noreply.github.com>
2025-01-20 12:36:24 +0000

    feat: add AcceptPatchManagementDisclaimer example for Jamf Pro client

---
+++ examples/patch_management/AcceptPatchManagementDisclaimer/AcceptPatchManagementDisclaimer.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
+
+	// Initialize the Jamf Pro client with the HTTP client configuration
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	err = client.AcceptPatchManagementDisclaimer()
+	if err != nil {
+		log.Fatalf("Error accepting patch management disclaimer: %v", err)
+	}
+
+	fmt.Println("Successfully accepted patch management disclaimer")
+}
