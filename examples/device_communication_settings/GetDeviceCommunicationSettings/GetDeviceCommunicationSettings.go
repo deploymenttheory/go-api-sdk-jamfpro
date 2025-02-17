2b09dcddd439d1138b7a460bf3dd8af05df2024e
ShocOne <62835948+ShocOne@users.noreply.github.com>
2025-02-17 13:46:32 +0000

    feat: added examples for device communication settings

--- examples/device_communication_settings/GetDeviceCommunicationSettings/GetDeviceCommunicationSettings.go
+++ examples/device_communication_settings/GetDeviceCommunicationSettings/GetDeviceCommunicationSettings.go
@@ -1,2 +1,32 @@
-// TODO
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
+
+	// Initialize the Jamf Pro client with the HTTP client configuration
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	clientChecinInfo, err := client.GetDeviceCommunicationSettings()
+	if err != nil {
+		log.Fatalf("Failed to get client checkin info, %v", err)
+	}
+
+	checkinInfoJson, err := json.MarshalIndent(clientChecinInfo, "", "    ")
+	if err != nil {
+		log.Fatalf("Failed to marshal json, %v", err)
+	}
+
+	fmt.Println(string(checkinInfoJson))
+}
--- examples/device_communication_settings/UpdateDeviceCommunicationSettings/UpdateDeviceCommunicationSettings.go
+++ examples/device_communication_settings/UpdateDeviceCommunicationSettings/UpdateDeviceCommunicationSettings.go
@@ -1,2 +1,41 @@
-// TODO
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
+
+	// Initialize the Jamf Pro client with the HTTP client configuration
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	settingsUpdate := jamfpro.ResourceDeviceCommunicationSettings{
+		AutoRenewMobileDeviceMdmProfileWhenCaRenewed:                  true,
+		AutoRenewMobileDeviceMdmProfileWhenDeviceIdentityCertExpiring: true,
+		AutoRenewComputerMdmProfileWhenCaRenewed:                      true,
+		AutoRenewComputerMdmProfileWhenDeviceIdentityCertExpiring:     true,
+		MdmProfileMobileDeviceExpirationLimitInDays:                   180, // can be 90 / 120 / 180
+		MdmProfileComputerExpirationLimitInDays:                       180, // can be 90 / 120 / 180
+	}
+
+	clientChecinInfo, err := client.UpdateDeviceCommunicationSettings(settingsUpdate)
+	if err != nil {
+		log.Fatalf("Failed to get client checkin info, %v", err)
+	}
+
+	checkinInfoJson, err := json.MarshalIndent(clientChecinInfo, "", "    ")
+	if err != nil {
+		log.Fatalf("Failed to marshal json, %v", err)
+	}
+
+	fmt.Println(string(checkinInfoJson))
+}
