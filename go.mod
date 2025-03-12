720ee34befe8fc667f60906aa2325826f1fca216
Joseph <joelittle11@live.com>
2025-03-12 14:00:41 +0000

    Added package upload timeout modifier to sdk func

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ toolchain go1.23.6
 
 // Deploymenttheory
 require (
-	github.com/deploymenttheory/go-api-http-client v0.3.1-0.20250312135052-f957be7f7963
+	github.com/deploymenttheory/go-api-http-client v0.3.1-0.20250312135758-6008e7c92a15
 	github.com/deploymenttheory/go-api-http-client-integrations v0.0.13
 )
 
--- sdk/jamfpro/modifiers.go
+++ sdk/jamfpro/modifiers.go
@@ -2,6 +2,12 @@ package jamfpro
 
 import "time"
 
+// Modifies the HTTP client timeout time for longer requests (uploads)
 func (c *Client) ModifyHttpTimeout(newTimeout time.Duration) {
 	c.HTTP.ModifyHttpTimeout(newTimeout)
+}
+
+// Resets the timeout time back to 10s
+func (c *Client) ResetHttpTimeout() {
+	c.HTTP.ResetTimeout()
 }
