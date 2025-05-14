edbf61744bcaa4f40fd826aab54131aba2cc0609
Neil Martin <neilmartin83@icloud.com>
2025-05-14 21:35:36 +0100

    feat: add v2/engage support

---
+++ examples/engage_settings/UpdateEngageSettings/UpdateEngageSettings.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/neilmartin/GitHub/go-api-sdk-jamfpro/localtesting/config.json"
+
+	// Initialize the Jamf Pro client with the HTTP client configuration
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	settingsUpdate := jamfpro.ResourceEngageSettings{
+		IsEnabled: false,
+	}
+
+	engageInfo, err := client.UpdateEngageSettings(settingsUpdate)
+	if err != nil {
+		log.Fatalf("Failed to get engage info, %v", err)
+	}
+
+	engageInfoJson, err := json.MarshalIndent(engageInfo, "", "    ")
+	if err != nil {
+		log.Fatalf("Failed to marshal json, %v", err)
+	}
+
+	fmt.Println(string(engageInfoJson))
+}
