b9203f4d096c32264317c5b27b0a34743e4a9ee4
Neil Martin <neilmartin83@icloud.com>
2025-05-14 13:50:29 +0100

    fix: refactor Jamf Protect functions (#783)

--- examples/jamf_protect/DeleteJamfProtectIntegration/DeleteJamfProtectIntegration.go
+++ examples/jamf_protect/DeleteJamfProtectIntegration/DeleteJamfProtectIntegration.go
@@ -1,2 +1,28 @@
-// TODO
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/Shared/GitHub/go-api-sdk-jamfpro/localtesting/clientconfig.json"
+
+	// Initialize the Jamf Pro client with the HTTP client configuration
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	// Delete the Jamf Protect integration
+	err = client.DeleteJamfProtectIntegration()
+	if err != nil {
+		log.Fatalf("Error deleting Jamf Protect integration: %v", err)
+	}
+
+	// Print success message
+	fmt.Println("Successfully deleted Jamf Protect integration")
+}
--- sdk/jamfpro/jamfproapi_jamf_protect.go
+++ sdk/jamfpro/jamfproapi_jamf_protect.go
@@ -1,8 +1,4 @@
 // jamfproapi_jamf_protect.go
-// Jamf Pro Api - Jamf Protect
-// api reference: https://developer.jamf.com/jamf-pro/reference/get_v1-jamf-protect
-// Jamf Pro API requires the structs to support an JSON data structure.
-
 package jamfpro
 
 import (
@@ -12,9 +8,32 @@ import (
 	"github.com/mitchellh/mapstructure"
 )
 
-const uriJamfProtect = "/api/v1/jamf-protect"
+const (
+	uriJamfProtect            = "/api/v1/jamf-protect"
+	uriJamfProtectSync        = "/api/v1/jamf-protect/plans/sync"
+	uriJamfProtectRegister    = "/api/v1/jamf-protect/register"
+	uriJamfProtectDeployments = "/api/v1/jamf-protect/deployments"
+	uriJamfProtectHistory     = "/api/v1/jamf-protect/history"
+	uriJamfProtectPlans       = "/api/v1/jamf-protect/plans"
+)
 
 // Response Structs
+type ResponseJamfProtectSettings struct {
+	ID             string `json:"id"`
+	ProtectURL     string `json:"protectUrl"`
+	SyncStatus     string `json:"syncStatus"`
+	APIClientID    string `json:"apiClientId"`
+	AutoInstall    bool   `json:"autoInstall"`
+	LastSyncTime   string `json:"lastSyncTime"`
+	APIClientName  string `json:"apiClientName"`
+	RegistrationID string `json:"registrationId"`
+}
+
+type ResponseJamfProtectDeploymentTasksList struct {
+	Results    []ResourceJamfProtectDeploymentTask `json:"results"`
+	TotalCount int                                 `json:"totalCount"`
+}
+
 type ResponseJamfProtectHistoryList struct {
 	TotalCount int                          `json:"totalCount"`
 	Results    []ResourceJamfProtectHistory `json:"results"`
@@ -26,23 +45,21 @@ type ResponseJamfProtectPlansList struct {
 }
 
 // Resource Structs
-type ResourceJamfProtectIntegrationSettings struct {
-	ID             string `json:"id"`
-	APIClientID    string `json:"apiClientId"`
-	APIClientName  string `json:"apiClientName"`
-	RegistrationID string `json:"registrationId"`
-	ProtectURL     string `json:"protectUrl"`
-	LastSyncTime   string `json:"lastSyncTime"`
-	SyncStatus     string `json:"syncStatus"`
-	AutoInstall    bool   `json:"autoInstall"`
+type ResourceJamfProtectDeploymentTask struct {
+	ID           string `json:"id"`
+	Status       string `json:"status"`
+	Updated      string `json:"updated"`
+	Version      string `json:"version"`
+	ComputerID   string `json:"computerId"`
+	ComputerName string `json:"computerName"`
 }
 
-type ResourceJamfProtectIntegrationRequest struct {
-	AutoInstall bool `json:"autoInstall"`
-}
-
-type ResourceJamfProtectRetryRequest struct {
-	IDs []string `json:"ids"`
+type ResourceJamfProtectHistory struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Date     string `json:"date"`
+	Note     string `json:"note"`
+	Details  string `json:"details"`
 }
 
 type ResourceJamfProtectPlan struct {
@@ -55,42 +72,37 @@ type ResourceJamfProtectPlan struct {
 	ScopeDescription string `json:"scopeDescription"`
 }
 
-type ResourceJamfProtectHistory struct {
-	ID       int    `json:"id"`
-	Username string `json:"username"`
-	Date     string `json:"date"`
-	Note     string `json:"note"`
-	Details  string `json:"details"`
-}
-
-// Structs for Jamf Protect API registration
-type ResourceJamfProtectRegisterRequest struct {
+// Request Structs
+type ResourceJamfProtectRegistration struct {
 	ProtectURL string `json:"protectUrl"`
 	ClientID   string `json:"clientId"`
 	Password   string `json:"password"`
 }
 
-type ResourceJamfProtectRegisterResponse struct {
-	ID             string `json:"id"`
-	APIClientID    string `json:"apiClientId"`
-	APIClientName  string `json:"apiClientName"`
-	RegistrationID string `json:"registrationId"`
-	ProtectURL     string `json:"protectUrl"`
-	LastSyncTime   string `json:"lastSyncTime"`
-	SyncStatus     string `json:"syncStatus"`
-	AutoInstall    bool   `json:"autoInstall"`
+type ResourceJamfProtectSettings struct {
+	AutoInstall bool `json:"autoInstall"`
 }
 
-// CRUD
+type ResourceJamfProtectDeploymentRequest struct {
+	PlanID          string   `json:"planId"`
+	TargetComputers []string `json:"targetComputers"`
+}
 
-// GetJamfProtectIntegrationSettings retrieves the current Jamf Protect integration settings
-func (c *Client) GetJamfProtectIntegrationSettings() (*ResourceJamfProtectIntegrationSettings, error) {
+type ResourceJamfProtectHistoryNote struct {
+	Note    string `json:"note"`
+	Details string `json:"details"`
+}
+
+// CRUD Operations
+
+// GetJamfProtectSettings retrieves the current Jamf Protect integration settings
+func (c *Client) GetJamfProtectSettings() (*ResponseJamfProtectSettings, error) {
 	endpoint := uriJamfProtect
-	var out ResourceJamfProtectIntegrationSettings
+	var out ResponseJamfProtectSettings
+
 	resp, err := c.HTTP.DoRequest("GET", endpoint, nil, &out)
-
 	if err != nil {
-		return nil, fmt.Errorf(errMsgFailedGet, "jamf protect integration settings", err)
+		return nil, fmt.Errorf(errMsgFailedGet, "jamf protect settings", err)
 	}
 
 	if resp != nil && resp.Body != nil {
@@ -100,14 +112,14 @@ func (c *Client) GetJamfProtectIntegrationSettings() (*ResourceJamfProtectIntegr
 	return &out, nil
 }
 
-// UpdateJamfProtectIntegrationSettings updates the Jamf Protect integration settings
-func (c *Client) UpdateJamfProtectIntegrationSettings(updatedSettings ResourceJamfProtectIntegrationSettings) (*ResourceJamfProtectIntegrationSettings, error) {
+// UpdateJamfProtectSettings updates the Jamf Protect integration settings
+func (c *Client) UpdateJamfProtectSettings(settings ResourceJamfProtectSettings) (*ResponseJamfProtectSettings, error) {
 	endpoint := uriJamfProtect
-	var out ResourceJamfProtectIntegrationSettings
-	resp, err := c.HTTP.DoRequest("PUT", endpoint, updatedSettings, &out)
+	var out ResponseJamfProtectSettings
 
+	resp, err := c.HTTP.DoRequest("PUT", endpoint, settings, &out)
 	if err != nil {
-		return nil, fmt.Errorf(errMsgFailedUpdate, "jamf protect integration settings", err)
+		return nil, fmt.Errorf(errMsgFailedUpdate, "jamf protect settings", err)
 	}
 
 	if resp != nil && resp.Body != nil {
@@ -117,21 +129,167 @@ func (c *Client) UpdateJamfProtectIntegrationSettings(updatedSettings ResourceJa
 	return &out, nil
 }
 
-// CreateJamfProtectIntegration creates a new Jamf Protect integration
-func (c *Client) CreateJamfProtectIntegration(autoInstall bool) (*ResourceJamfProtectIntegrationSettings, error) {
-	endpoint := uriJamfProtect
-	requestBody := ResourceJamfProtectIntegrationRequest{
+// RegisterJamfProtect registers a new Jamf Protect integration
+func (c *Client) RegisterJamfProtect(registration ResourceJamfProtectRegistration) (*ResponseJamfProtectSettings, error) {
+	endpoint := uriJamfProtectRegister
+	var out ResponseJamfProtectSettings
+
+	resp, err := c.HTTP.DoRequest("POST", endpoint, registration, &out)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgFailedCreate, "jamf protect registration", err)
+	}
+
+	if resp != nil && resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	return &out, nil
+}
+
+// SyncJamfProtectPlans initiates a sync of Jamf Protect plans
+func (c *Client) SyncJamfProtectPlans() error {
+	endpoint := uriJamfProtectSync
+
+	resp, _ := c.HTTP.DoRequest("POST", endpoint, nil, nil)
+
+	if resp != nil && resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	if resp.StatusCode != 204 {
+		return fmt.Errorf("failed to sync Jamf Protect plans: unexpected status code %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+// CreateJamfProtectIntegration creates a complete Jamf Protect integration
+func (c *Client) CreateJamfProtectIntegration(registration ResourceJamfProtectRegistration, autoInstall bool) (*ResponseJamfProtectSettings, error) {
+	_, err := c.RegisterJamfProtect(registration)
+	if err != nil {
+		return nil, fmt.Errorf("failed to register Jamf Protect: %v", err)
+	}
+
+	settings := ResourceJamfProtectSettings{
 		AutoInstall: autoInstall,
 	}
-	var out ResourceJamfProtectIntegrationSettings
-	resp, err := c.HTTP.DoRequest("PUT", endpoint, requestBody, &out)
 
+	updateResp, err := c.UpdateJamfProtectSettings(settings)
 	if err != nil {
-		return nil, fmt.Errorf(errMsgFailedCreate, "jamf protect integration", err)
+		return nil, fmt.Errorf("failed to update settings: %v", err)
+	}
+
+	err = c.SyncJamfProtectPlans()
+	if err != nil {
+		return nil, fmt.Errorf("failed to sync plans: %v", err)
+	}
+
+	return updateResp, nil
+}
+
+// GetJamfProtectDeploymentTasks retrieves a list of tasks for a specific deployment
+func (c *Client) GetJamfProtectDeploymentTasks(deploymentID string, params url.Values) (*ResponseJamfProtectDeploymentTasksList, error) {
+	endpoint := fmt.Sprintf("%s/%s/tasks", uriJamfProtectDeployments, deploymentID)
+	var out ResponseJamfProtectDeploymentTasksList
+
+	resp, err := c.DoPaginatedGet(endpoint, params)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgFailedPaginatedGet, "jamf protect deployment tasks", err)
+	}
+
+	out.TotalCount = resp.Size
+
+	for _, value := range resp.Results {
+		var newObj ResourceJamfProtectDeploymentTask
+		err := mapstructure.Decode(value, &newObj)
+		if err != nil {
+			return nil, fmt.Errorf(errMsgFailedMapstruct, "jamf protect deployment task", err)
+		}
+		out.Results = append(out.Results, newObj)
+	}
+
+	return &out, nil
+}
+
+// RetryJamfProtectDeploymentTasks requests a retry of Protect install tasks for a specific deployment
+func (c *Client) RetryJamfProtectDeploymentTasks(deploymentID string) error {
+	endpoint := fmt.Sprintf("%s/%s/tasks/retry", uriJamfProtectDeployments, deploymentID)
+
+	resp, err := c.HTTP.DoRequest("POST", endpoint, nil, nil)
+	if err != nil {
+		return fmt.Errorf(errMsgFailedCreate, "retry jamf protect deployment tasks", err)
 	}
 
 	if resp != nil && resp.Body != nil {
 		defer resp.Body.Close()
+	}
+
+	if resp.StatusCode != 204 {
+		return fmt.Errorf("failed to retry jamf protect deployment tasks: unexpected status code %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+// GetJamfProtectHistory retrieves the history of Jamf Protect actions
+func (c *Client) GetJamfProtectHistory(params url.Values) (*ResponseJamfProtectHistoryList, error) {
+	endpoint := uriJamfProtectHistory
+
+	resp, err := c.DoPaginatedGet(endpoint, params)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgFailedPaginatedGet, "jamf protect history", err)
+	}
+
+	var out ResponseJamfProtectHistoryList
+	out.TotalCount = resp.Size
+
+	for _, value := range resp.Results {
+		var newObj ResourceJamfProtectHistory
+		err := mapstructure.Decode(value, &newObj)
+		if err != nil {
+			return nil, fmt.Errorf(errMsgFailedMapstruct, "jamf protect history", err)
+		}
+		out.Results = append(out.Results, newObj)
+	}
+
+	return &out, nil
+}
+
+// CreateJamfProtectHistoryNote adds a note to the Jamf Protect history
+func (c *Client) CreateJamfProtectHistoryNote(note ResourceJamfProtectHistoryNote) error {
+	endpoint := uriJamfProtectHistory
+
+	resp, err := c.HTTP.DoRequest("POST", endpoint, note, nil)
+	if err != nil {
+		return fmt.Errorf(errMsgFailedCreate, "jamf protect history note", err)
+	}
+
+	if resp != nil && resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	return nil
+}
+
+// GetJamfProtectPlans retrieves all Jamf Protect plans
+func (c *Client) GetJamfProtectPlans(params url.Values) (*ResponseJamfProtectPlansList, error) {
+	endpoint := uriJamfProtectPlans
+
+	resp, err := c.DoPaginatedGet(endpoint, params)
+	if err != nil {
+		return nil, fmt.Errorf(errMsgFailedPaginatedGet, "jamf protect plans", err)
+	}
+
+	var out ResponseJamfProtectPlansList
+	out.TotalCount = resp.Size
+
+	for _, value := range resp.Results {
+		var newObj ResourceJamfProtectPlan
+		err := mapstructure.Decode(value, &newObj)
+		if err != nil {
+			return nil, fmt.Errorf(errMsgFailedMapstruct, "jamf protect plan", err)
+		}
+		out.Results = append(out.Results, newObj)
 	}
 
 	return &out, nil
@@ -151,109 +309,4 @@ func (c *Client) DeleteJamfProtectIntegration() error {
 	}
 
 	return nil
-}
-
-// New function to request a retry of Protect install tasks
-func (c *Client) RetryJamfProtectInstallTasks(deploymentID string, taskIDs []string) error {
-	endpoint := fmt.Sprintf("%s/deployments/%s/tasks/retry", uriJamfProtect, deploymentID)
-
-	requestBody := ResourceJamfProtectRetryRequest{
-		IDs: taskIDs,
-	}
-
-	resp, err := c.HTTP.DoRequest("POST", endpoint, requestBody, nil)
-	if err != nil {
-		return fmt.Errorf(errMsgFailedUpdate, "retry Jamf Protect install tasks", err)
-	}
-
-	if resp != nil && resp.Body != nil {
-		defer resp.Body.Close()
-	}
-
-	return nil
-}
-
-// GetJamfProtectHistory retrieves the history of Jamf Protect actions
-func (c *Client) GetJamfProtectHistory(params url.Values) (*ResponseJamfProtectHistoryList, error) {
-	endpoint := fmt.Sprintf("%s/history", uriJamfProtect)
-
-	resp, err := c.DoPaginatedGet(endpoint, params)
-	if err != nil {
-		return nil, fmt.Errorf(errMsgFailedPaginatedGet, "Jamf Protect history", err)
-	}
-
-	var out ResponseJamfProtectHistoryList
-	out.TotalCount = resp.Size
-
-	for _, value := range resp.Results {
-		var newObj ResourceJamfProtectHistory
-		err := mapstructure.Decode(value, &newObj)
-		if err != nil {
-			return nil, fmt.Errorf(errMsgFailedMapstruct, "Jamf Protect history", err)
-		}
-		out.Results = append(out.Results, newObj)
-	}
-
-	return &out, nil
-}
-
-// GetJamfProtectPlans retrieves all previously synced Jamf Protect Plans with their associated configuration profile information
-func (c *Client) GetJamfProtectPlans(params url.Values) (*ResponseJamfProtectPlansList, error) {
-	endpoint := fmt.Sprintf("%s/plans", uriJamfProtect)
-
-	resp, err := c.DoPaginatedGet(endpoint, params)
-	if err != nil {
-		return nil, fmt.Errorf(errMsgFailedPaginatedGet, "Jamf Protect plans", err)
-	}
-
-	var out ResponseJamfProtectPlansList
-	out.TotalCount = resp.Size
-
-	for _, value := range resp.Results {
-		var newObj ResourceJamfProtectPlan
-		err := mapstructure.Decode(value, &newObj)
-		if err != nil {
-			return nil, fmt.Errorf(errMsgFailedMapstruct, "Jamf Protect plan", err)
-		}
-		out.Results = append(out.Results, newObj)
-	}
-
-	return &out, nil
-}
-
-// SyncJamfProtectPlans syncs plans with Jamf Protect
-func (c *Client) SyncJamfProtectPlans() error {
-	endpoint := fmt.Sprintf("%s/plans/sync", uriJamfProtect)
-
-	resp, _ := c.HTTP.DoRequest("POST", endpoint, nil, nil)
-	if resp == nil {
-		return fmt.Errorf("failed to sync Jamf Protect plans: no response received")
-	}
-
-	if resp.Body != nil {
-		defer resp.Body.Close()
-	}
-
-	if resp.StatusCode != 204 {
-		return fmt.Errorf("failed to sync Jamf Protect plans: unexpected status code %d", resp.StatusCode)
-	}
-
-	return nil
-}
-
-// CreateJamfProtectAPIConfiguration registers a Jamf Protect API configuration with Jamf Pro
-func (c *Client) CreateJamfProtectAPIConfiguration(config ResourceJamfProtectRegisterRequest) (*ResourceJamfProtectRegisterResponse, error) {
-	endpoint := fmt.Sprintf("%s/register", uriJamfProtect)
-
-	var response ResourceJamfProtectRegisterResponse
-	resp, err := c.HTTP.DoRequest("POST", endpoint, config, &response)
-	if err != nil {
-		return nil, fmt.Errorf(errMsgFailedCreate, "register Jamf Protect API configuration", err)
-	}
-
-	if resp != nil && resp.Body != nil {
-		defer resp.Body.Close()
-	}
-
-	return &response, nil
 }
