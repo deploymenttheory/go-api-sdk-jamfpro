c1ba6722f7decc5947a5ae95bf7c5fcce8da3010
Neil Martin <neilmartin83@icloud.com>
2025-07-26 18:56:23 +0100

    refactor: Port Mobile Device Extension Attribute handling to Jamf Pro API /v1/mobile-device-extension-attributes

--- examples/mobile_device_extension_attributes/CreateMobileDeviceExtensionAttribute/CreateMobileDeviceExtensionAttribute.go
+++ examples/mobile_device_extension_attributes/CreateMobileDeviceExtensionAttribute/CreateMobileDeviceExtensionAttribute.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	// Define the path to the JSON configuration file
-	configFilePath := "/Users/dafyddwatkins/localtesting/jamfpro/clientconfig.json"
+	configFilePath := "/Users/Shared/GitHub/go-api-sdk-jamfpro/localtesting/clientconfig.json"
 
 	// Initialize the Jamf Pro client with the HTTP client configuration
 	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
@@ -18,18 +18,16 @@ func main() {
 	}
 
 	// Define a new mobile device extension attribute
-	newAttribute := &jamfpro.ResourceMobileExtensionAttribute{
-		Name:             "New Attribute",
-		Description:      "This is a test attribute",
-		DataType:         "String",
-		InventoryDisplay: "General",
-		InputType: jamfpro.MobileExtensionAttributeSubsetInputType{
-			Type: "Text Field",
-		},
+	newAttribute := &jamfpro.ResourceMobileDeviceExtensionAttribute{
+		Name:                 "New Attribute",
+		Description:          "This is a test attribute",
+		DataType:             "String",
+		InventoryDisplayType: "General",
+		InputType:            "TEXT",
 	}
 
 	// Create the new attribute
-	createdAttribute, err := client.CreateMobileExtensionAttribute(newAttribute)
+	createdAttribute, err := client.CreateMobileDeviceExtensionAttribute(newAttribute)
 	if err != nil {
 		log.Fatalf("Error creating mobile device extension attribute: %v", err)
 	}
