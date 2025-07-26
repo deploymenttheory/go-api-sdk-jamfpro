3ab3faa0cb95f7f6543550f5f5ff8e38a9ba15b9
Neil Martin <neilmartin83@icloud.com>
2025-07-26 20:14:05 +0100

    feat: Add new and missing fields for /v4/enrollment

--- sdk/jamfpro/jamfproapi_enrollment.go
+++ sdk/jamfpro/jamfproapi_enrollment.go
@@ -72,6 +72,12 @@ type ResourceEnrollment struct {
 	SigningMdmProfileEnabled                     bool                           `json:"signingMdmProfileEnabled"`
 	MdmSigningCertificate                        *ResourceEnrollmentCertificate `json:"mdmSigningCertificate"`
 	RestrictReenrollment                         bool                           `json:"restrictReenrollment"`
+	FlushLocationInformation                     bool                           `json:"flushLocationInformation"`
+	FlushLocationHistoryInformation              bool                           `json:"flushLocationHistoryInformation"`
+	FlushPolicyHistory                           bool                           `json:"flushPolicyHistory"`
+	FlushExtensionAttributes                     bool                           `json:"flushExtensionAttributes"`
+	FlushSoftwareUpdatePlans                     bool                           `json:"flushSoftwareUpdatePlans"`
+	FlushMdmCommandsOnReenroll                   string                         `json:"flushMdmCommandsOnReenroll"`
 	MacOsEnterpriseEnrollmentEnabled             bool                           `json:"macOsEnterpriseEnrollmentEnabled"`
 	ManagementUsername                           string                         `json:"managementUsername"`
 	CreateManagementAccount                      bool                           `json:"createManagementAccount"`
@@ -91,6 +97,7 @@ type ResourceEnrollment struct {
 	AccountDrivenDeviceMacosEnrollmentEnabled    bool                           `json:"accountDrivenDeviceMacosEnrollmentEnabled"`
 	AccountDrivenUserVisionosEnrollmentEnabled   bool                           `json:"accountDrivenUserVisionosEnrollmentEnabled"`
 	AccountDrivenDeviceVisionosEnrollmentEnabled bool                           `json:"accountDrivenDeviceVisionosEnrollmentEnabled"`
+	MaidUsernameMergeEnabled                     bool                           `json:"maidUsernameMergeEnabled"`
 }
 
 // Resource struct for enrollment language messaging
