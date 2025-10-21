3fb8b110c1a148c03c2bcb1721ed409e5af89215
Neil Martin <neil.martin@jamf.com>
2025-10-21 18:22:48 +0100

    feat: add example for fetching Smart Computer Group by ID

---
+++ examples/smart_computer_groups/GetSmartComputerGroupByIDV2/GetSmartComputerGroupByIDV2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-jamfpro/sdk/jamfpro"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/Shared/GitHub/go-api-sdk-jamfpro/localtesting/clientconfig.json"
+
+	// Initialize the Jamf Pro client with the HTTP client configuration
+	client, err := jamfpro.BuildClientWithConfigFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to initialize Jamf Pro client: %v", err)
+	}
+
+	// Define a Smart Computer Group ID for testing
+	groupID := "1197" // Replace with actual group ID
+
+	// Call GetSmartComputerGroupByID function
+	group, err := client.GetSmartComputerGroupByIDV2(groupID)
+	if err != nil {
+		log.Fatalf("Error fetching Smart Computer Group by ID: %v", err)
+	}
+
+	// Pretty print the group in JSON
+	groupJSON, err := json.MarshalIndent(group, "", "    ") // Indent with 4 spaces
+	if err != nil {
+		log.Fatalf("Error marshaling Smart Computer Group data: %v", err)
+	}
+	fmt.Println("Fetched Smart Computer Group:\n", string(groupJSON))
+}
